91a8bb7f9fc777950117f150a194f48819e35847
Chouaib Boubekeur <33073278+thbob2@users.noreply.github.com>
2024-07-27 23:18:25 +0100

    project int

---
+++ server/contracts/CarSupplyChain.sol
@@ -0,0 +1,29 @@
+// contracts/CarSupplyChain.sol
+pragma solidity ^0.8.0;
+
+contract CarSupplyChain {
+    struct Car {
+        uint256 id;
+        string model;
+        address manufacturer;
+        address supplier;
+        address dealer;
+        uint256 timestamp;
+    }
+
+    mapping(uint256 => Car) public cars;
+    uint256 public carCount;
+
+    function registerCar(string memory _model) public {
+        carCount++;
+        cars[carCount] = Car(carCount, _model, msg.sender, address(0), address(0), block.timestamp);
+    }
+
+    function transferOwnership(uint256 _id, address _newOwner) public {
+        Car storage car = cars[_id];
+        require(msg.sender == car.supplier || msg.sender == car.dealer, "Not authorized");
+        if (msg.sender == car.supplier) {
+            car.dealer = _newOwner;
+        }
+    }
+}
